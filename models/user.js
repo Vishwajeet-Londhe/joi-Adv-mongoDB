b273127a295c58e50db013814a6190920f6411ea
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-29 00:13:30 +0530

    layer 1 protection

--- models/user.js
+++ models/user.js
@@ -16,15 +16,38 @@
 
 // joi
 
+const { required } = require('joi');
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/joitestdb");
 
 const userSchema = mongoose.Schema({
-    username:String,
-    name:String,
-    age:Number,
-    contact:Number,
-    email:String
+    username:{
+        type:String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+        unique: true
+    },
+    name:{
+        type:String,
+        required: true,
+        minlength: 3,
+        maxlength: 100,
+        required: true
+    },
+    age:{
+        type:Number,
+        min: 18,
+        required: true
+    },
+    contact:{
+        type:Number,
+        minlength:10,
+        required: true
+    },
+    email:{
+        type:String
+    },
 });
 
 module.exports = mongoose.model('User',userSchema);
