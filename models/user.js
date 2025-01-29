31e1f607972a763325a0d7ee967a642f13514366
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-29 19:54:34 +0530

    joi data validate

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@
 
 // joi
 
-const { required } = require('joi');
+const JOI = require("joi");
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/joitestdb");
 
@@ -49,5 +49,15 @@ const userSchema = mongoose.Schema({
         type:String
     },
 });
+
+function validateModel(data){
+    let Schema = Joi.object({
+        username: Joi.string().min(5).max(50).required().alphanum(),
+        name: Joi.string().min(3).max(100).required(),
+        age: Joi.number().min(18).required(),
+        contact: Joi.number().min(10).required(),
+        email: Joi.string().email()
+    })
+}
 
 module.exports = mongoose.model('User',userSchema);
